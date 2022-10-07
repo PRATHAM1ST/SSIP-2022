45afc85df1b013a81b8069000c055fefb7b663c9
Jay Shah <jaysshah189@gmail.com>
2022-10-07 22:51:23 +0530

    correct otp

--- backend/qrcodeproject/qrweb/urls.py
+++ backend/qrcodeproject/qrweb/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from. import views
 
 urlpatterns = [
-    path('',views.generate_otp,name='otp_generate')
+    path('/getotp',views.generate_otp,name='otp_generate'),
+    path('',views.is_correct,name='correct_otp'),
 ]
--- backend/qrcodeproject/qrweb/views.py
+++ backend/qrcodeproject/qrweb/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render,HttpResponse
 from django_otp.oath import totp
 import time
-
+import json
 
 # Create your views here
 
 def generate_otp(request):
     secret_key = b'12345678901234567890'
     now = int(time.time())
-    for delta in range(10,110,20):
-        print(totp(key=secret_key, step=10, digits=6, t0=(now-delta)))
+    x = totp(key=secret_key, step=10, digits=6, t0=(now-10))
+    print(x)
     response = {
         'status': 'success',
     }
-    return HttpResponse(response['status'])
+    return HttpResponse(json.dumps(response))
+
+def is_correct(request):
+    ans = {
+        'status1': 'Correct',
+        'status2': 'incorrect',
+    }
+    secret_key = b'12345678901234567890'
+    now = int(time.time())
+    x = totp(key=secret_key, step=10, digits=6, t0=(now-10))
+    if x==request.GET['OTP']:
+        return HttpResponse(json.dumps(ans['status1']))
+    else:
+        return HttpResponse(json.dumps(ans['status2']))
